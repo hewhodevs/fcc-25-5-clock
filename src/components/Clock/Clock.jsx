40dd5d9a27a02694dd745bbcb3c110877d135575
hewhodevs <hewhodevs@outlook.com>
2022-03-15 20:38:28 +1030

    initial project setup in react

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Clock.css"
+
+class Clock extends React.Component {
+
+  render() {
+    return(
+      <div className="ClockTwentyFive">
+        <h1>Clock here!</h1>
+      </div>
+    );
+  }
+}
+
+export default Clock;
